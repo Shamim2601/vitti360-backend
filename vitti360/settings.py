1def2ef74150fa65b321e10f360cdd480fa6a880
Shamim2601 <shamim2601@gmail.com>
2024-11-13 22:14:11 +0600

    access token lifetime to 2 days

--- vitti360/settings.py
+++ vitti360/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     # Optional: Enable token rotation and blacklisting
     'ROTATE_REFRESH_TOKENS': True,
